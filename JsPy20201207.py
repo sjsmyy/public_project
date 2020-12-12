02c8f6797d830fa4efdab14238e77a655149f836
Jsshi <>
2020-12-12 14:08:44 +0800

    20201212

--- JsPy20201207.py
+++ JsPy20201207.py
@@ -15,13 +15,13 @@ def pySum(n):
     a = CreateList(n, 2)
     b = CreateList(n, 3)
     c = []
-    print(a)
-    print(b)
+   # print(a)
+   # print(b)
     for i in range(len(a)):
         c.append(a[i]+b[i])
     return c
 c = pySum(10)
-print(c)
+# print(c)
 
 # NumPy方法求的二、三次方的列表和平方的列表的合集
 import numpy as np
@@ -31,8 +31,30 @@ def NumSumPy(n):
     c = a + b
     return c
 c = NumSumPy(10)
-print(c)
+# print(c)
 
 # 创建一维数组
 d = np.array([1, 2, 3, 4])
-print(d[2])
+# print(d[2])
+
+# 创建多维数组
+d = np.array([np.array([1, 2, 3, 4]), np.array([5, 6, 7, 8])])
+# print(d.shape)
+
+# 作业：
+# 创建类Restaurant()
+class Restaurant():
+    def __init__(self,c_name, c_type):
+        self.restaurant_name = c_name
+        self.cuisine_type = c_type
+    def describe_restaurant(self):
+        print("餐馆的名称是：", self.restaurant_name)
+        print("餐馆的种类是：", self.cuisine_type)
+    def open_status(self):
+        print("餐馆正在营业")
+# 创建对象
+restaurant1 = Restaurant("海底捞", "火锅")
+print(restaurant1.restaurant_name)
+print(restaurant1.cuisine_type)
+restaurant1.open_status()
+restaurant1.describe_restaurant()
